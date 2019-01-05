c31cf4b0187eec85ca50a991f483bf8f714b2973
mpissanos <mpissano@gmail.com>
2019-01-05 11:11:11 -0500

    added captain and user models

---
+++ app/models/captain.rb
@@ -0,0 +1,6 @@
+class Captain < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
